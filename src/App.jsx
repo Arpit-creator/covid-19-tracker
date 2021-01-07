77d9e0834583816c1366752b1f620609c780f347
arpitchakladar <arpitchakladar@gmail.com>
2021-01-07 12:56:52 +0530

    Switched to tabs

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,41 @@ import Cards from 'components/Cards/Cards';
 import Chart from 'components/Chart/Chart';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [dailyData, setDailyData] = useState(null);
-  const [countries, setCountries] = useState(null);
-  const [country, setCountry] = useState(null);
+	const [data, setData] = useState(null);
+	const [dailyData, setDailyData] = useState(null);
+	const [countries, setCountries] = useState(null);
+	const [country, setCountry] = useState(null);
 
-  useEffect(() => {
-    fetchData(country).then(data => {
-      setData(data);
-    });
-    fetchDailyData().then(data => {
-      setDailyData(data);
-    });
-    fetchCountries().then(countries => {
-      setCountries(countries);
-    });
-  }, [country]);
+	useEffect(() => {
+		fetchData(country).then(data => {
+			setData(data);
+		});
+		fetchDailyData().then(data => {
+			setDailyData(data);
+		});
+		fetchCountries().then(countries => {
+			setCountries(countries);
+		});
+	}, [country]);
 
-  return (
-    <div className="App">
-      <img src={Logo} alt="" className="logo" />
-      {data && dailyData && countries
-        ? <>
-          <Cards data={data} />
-          <select className="country" onChange={({ target: { value } }) => setCountry(value.toLowerCase() === 'global' ? null : value.toLowerCase())}>
-            <option value="global">Global</option>
-            {countries.map(country => <option value={country} key={country}>{country}</option> )}
-          </select>
-          <Chart dailyData={dailyData} data={data} country={country} />
-        </>
-        : <div className="loading">
-          <img src={LoadingGif} alt="" />
-       </div>
-      }
-    </div>
-  );
+	return (
+		<div className="App">
+			<img src={Logo} alt="" className="logo" />
+			{data && dailyData && countries
+				? <>
+					<Cards data={data} />
+					<select className="country" onChange={({ target: { value } }) => setCountry(value.toLowerCase() === 'global' ? null : value.toLowerCase())}>
+						<option value="global">Global</option>
+						{countries.map(country => <option value={country} key={country}>{country}</option> )}
+					</select>
+					<Chart dailyData={dailyData} data={data} country={country} />
+				</>
+				: <div className="loading">
+					<img src={LoadingGif} alt="" />
+			 </div>
+			}
+		</div>
+	);
 };
 
 export default App;
